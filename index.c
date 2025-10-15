54c5732b3aa053467e0825a435344df54f421da7
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-10-15 19:43:07 -0300

    feat: criação dos objetos básicos para implementação e iniciação da aplicação no arquivo index.c

---
+++ index.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/* Criação dos objetos para o programa */
+
+struct Clientes{
+    int id;
+    char nome[50];
+    double cpf;
+    char sexo;
+    struct Telefone tel;
+};
+
+struct Telefone{
+    char fixo[15];
+    char movel[16];
+};
+
+struct Produtos{
+    int id;
+    char nome[20];
+    struct Modelos models;
+    double valor;
+};
+
+struct Modelos{
+    char marca[20];
+    char modelo[20];
+};
+
+struct Carrinho{
+    struct Clientes cliente;
+    struct Produtos produto[3];
+    int quantidade[3];
+    double subtotal;
+    double desconto;
+    double total;
+};
+
+//Função Principal
+int main(){
+}
+
